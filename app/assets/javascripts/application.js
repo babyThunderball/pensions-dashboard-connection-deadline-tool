f99b97c9cc695d5e230cf77b9ada908a5407d76a
JodiB-TPR <jodi.bennett@tpr.gov.uk>
2022-10-25 16:08:03 +0100

    Update application.js
    Added "Back to top" JS.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,16 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
+
+var backtotopscrollposition = 4 * window.innerHeight;
+
+$(document).scroll(function () {
+  var y = $(this).scrollTop();
+  if (y > backtotopscrollposition) {
+      
+          $('.app-back-to-top').fadeIn();
+      
+  } else {
+      $('.app-back-to-top').fadeOut();
+  }
+});
